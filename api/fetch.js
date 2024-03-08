3b9363bf9bba4ab782fabd9f3aba36987d634a7f
KayTee1 <truong.kha@outlook.com>
2024-03-08 23:47:29 +0200

    gitignore

--- api/fetch.js
+++ api/fetch.js
@@ -1,9 +1,9 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
+const { API_KEY } = require("./secret");
 
 const fetchData = async (query) => {
   const API_URL = `https://api.calorieninjas.com/v1/nutrition?query=${query}`;
-  const API_KEY = "yuYmYaFd385QP2Hda4JHwQ==c08UhxL8MWJGxcgM";
 
   const headers = new Headers();
   headers.append("X-Api-Key", API_KEY);
@@ -28,13 +28,17 @@ if (!query) {
 }
 
 const writeData = (data) => {
-  fs.writeFile("../macros/db/apiResult.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) {
-      console.error("Error writing data to file:", err);
-    } else {
-      console.log("Data has been written to ../db/apiResult.json");
+  fs.writeFile(
+    "../macros/db/apiResult.json",
+    JSON.stringify(data, null, 2),
+    (err) => {
+      if (err) {
+        console.error("Error writing data to file:", err);
+      } else {
+        console.log("Data has been written to ../db/apiResult.json");
+      }
     }
-  });
+  );
 };
 
 // Example usage:
